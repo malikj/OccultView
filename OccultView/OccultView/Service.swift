1bf6a3e6d62f9fc2643e3934bcf065a435905bf3
malikj <malikj@PRINHYLTPAP0637.local>
2018-06-09 12:24:29 +0530

    third commit with another file

---
+++ OccultView/OccultView/Service.swift
@@ -0,0 +1,19 @@
+//
+//  Service.swift
+//  OccultView
+//
+//  Created by malikj on 09/06/18.
+//  Copyright © 2018 malikj. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init() {
+        
+    }
+    public static func dosomething() -> String {
+        return "Did some stuff, here you go"
+    }
+}
