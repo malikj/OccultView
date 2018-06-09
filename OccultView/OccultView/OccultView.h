313d42cbbb489a11b4fa4f2095ed2a734aab0b94
malikj <malikj@PRINHYLTPAP0637.local>
2018-06-09 10:43:49 +0530

    Initial commit from machine

---
+++ OccultView/OccultView/OccultView.h
@@ -0,0 +1,19 @@
+//
+//  OccultView.h
+//  OccultView
+//
+//  Created by malikj on 09/06/18.
+//  Copyright © 2018 malikj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OccultView.
+FOUNDATION_EXPORT double OccultViewVersionNumber;
+
+//! Project version string for OccultView.
+FOUNDATION_EXPORT const unsigned char OccultViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OccultView/PublicHeader.h>
+
+
